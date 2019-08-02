6352a7b0617b7091569b9343507b9bd2dad43fc9
Manish <manish.perumalla@cgi.com>
2019-08-02 12:33:25 +0530

    Task3 Added

---
+++ src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
@@ -0,0 +1,12 @@
+package com.stackroute.trackservice.repository;
+
+import com.stackroute.trackservice.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Integer> {
+    @Query("select t from Track t where t.name = ?1")
+    Track getTrackByName(String name);
+}
